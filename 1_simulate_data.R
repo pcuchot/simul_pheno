3b0f5832eb414c872114d89734ca22600da34ee6
Paul Cuchot <paul.cuchot@gmail.com>
2023-04-21 17:09:01 +0200

    21/04/32

---
+++ 1_simulate_data.R
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------------------------
+# title : 
+# author : 
+# date : 
+# note : 
+# -----------------------------------------------------------------------------
+
+
+# Packages ----------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------
+
+# number of sites
+# number of year
+# number of capture session per site
+# number of breeding bird per site (catchable adults)
+# mean breeding date per site (vector)
+# for each year_site
+
+
