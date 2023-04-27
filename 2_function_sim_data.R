21a51a3f4df686d175e881d566fcbcb758c7259a
Paul CUCHOT <cuchot@newcefe.newage.fr>
2023-04-27 13:13:55 +0200

    27_04

--- 2_function_sim_data.R
+++ 2_function_sim_data.R
@@ -1,103 +1,25 @@
 # -----------------------------------------------------------------------------
-# title : 1_simulate_data
+# title : 2_function_sim_data
 # author : Paul Cuchot  
-# date : 25/04/2023
-# note : 
+# date : 27/04/2023
+# note : Create a function to simulate data per site 
 # -----------------------------------------------------------------------------
+
+# assumptions
+
+# -
+# -
+# -
+# -
+# -
+# -
+
+
 
 
 # Packages ----------------------------------------------------------------
 library(tidyverse)
 
-
-# -------------------------------------------------------------------------
-# 1 Estimate phenology without taking into account young dispersal --------
-# -------------------------------------------------------------------------
-
-# arguments ? 
-
-# n_session
-# n_breeders
-# n_years
-# mean_ld (per year)
-
-# 
-
-
-# number of capture session per site
-n_session <- 4
-
-# number of breeding birds per site (catchable adults)
-n_breeders <- 5
-
-# number of breeding attempts
-n_bredd_att <-  1
-
-# breeding date (distrib to sample for breeding dates)
-mean_ld <- 120
-sd_ld <- 10
-
-# sample n_breeders laying events
-ld_dates <- round(rnorm(n_breeders, mean = mean_ld, sd = sd_ld))
-
-# number of eggs per pair
-mean_eggs <- 8
-sd_eggs <- 5
-
-n_eggs <- abs(round(rnorm(n_breeders, mean_eggs, sd_eggs)))
-
-# fledglings dates 
-fledgl_dates <- round(ld_dates)+35
-
-# create a dataframe (one row per breeding pair)
-df_breed <- data.frame(
-  ld_dates = ld_dates,
-  n_eggs = n_eggs,
-  fledgl_dates = fledgl_dates
-)
-
-# Sample design -----------------------------------------------------------
-
-# French CES = 4 capture sessions from day 120 to 180
-t_capt <- round(seq(120,180, length.out = 4))
-
-# Number of sample individual per session (~capture effort)
-mean_n_capt <- 9
-
-# Dataframe with n_adults and n_juveniles captured per session
-df_session <- data.frame(t = t_capt,
-                         n_capt_adults = NA,
-                         n_capt_juveniles = NA) 
-
-for(i in t_capt){
-  # catchable adults
-  n_adults <- n_breeders * 2
-  # catchable juveniles
-  n_juveniles <- sum(df_breed[df_breed$fledgl_dates < i ,]$n_eggs)
-  
-  capt_indiv <- sample( # how many individuals captured
-    c(rep(0,n_adults), 
-      rep(1,n_juveniles)),
-    round(rnorm(1,mean_n_capt)), replace = TRUE)
-  
-  df_session[df_session$t ==i,"n_capt_adults"] <- sum(capt_indiv == 0)
-  df_session[df_session$t ==i,"n_capt_juveniles"] <- sum(capt_indiv == 1) 
-  
-}
-
-# calculate productivity per session
-df_session <- df_session%>%
-  mutate(prod = n_capt_juveniles/(n_capt_adults+n_capt_juveniles))
-
-df_session
-
-
-# Plot productivity -------------------------------------------------------
-
-df_session%>%
-  ggplot(aes(x = t, y = prod))+
-  geom_point()+
-  theme_light()
 
 
 # function to simulated data for a signe site, N times --------------------
@@ -184,7 +106,7 @@ simul_data <- function(n_breeders,
   
   return(list(capt_sess = df_site, 
               mean_ld_year = mean_ld))
-
+  
   
 }
 
@@ -195,6 +117,7 @@ data1 <- simul_data(n_breeders = 5,
 
 
 # plot productivity~day
+
 data1$capt_sess%>%
   ggplot(aes(x = t, y = prod, color = as.character(year)))+
   geom_point()+
