d00d48d323d3edae6676564320e38f8636938f8f
Paul CUCHOT <cuchot@newcefe.newage.fr>
2024-08-02 17:15:47 +0200

    maj 2_

--- 2_function_sim_data.R
+++ 2_function_sim_data.R
@@ -19,23 +19,29 @@
 
 # Packages ----------------------------------------------------------------
 require(tidyverse)
+require(truncnorm)
+
+# function to simulated data for a sigle site, N times --------------------
 
 
-# function to simulated data for a signe site, N times --------------------
-
-
-simul_data <- function(n_breeders,
-                       n_years, 
-                       n_session, 
-                       start_ces = 80,
-                       end_ces = 120,
-                       mean_ld_site = 90){
+simul_data <- function(n_breeders = 10, # number of pair
+                       n_years = 10, 
+                       n_session = 50, 
+                       start_ces = 100,
+                       end_ces = 200,
+                       mean_ld_site = 90,
+                       selection_stre_ld = -0.003,
+                       sd_ld = 7){
+  
   
   # mean laying date (among breeding individuals - change between years)
-  mean_ld <- round(rnorm(n_years, mean_ld_site, 15)) # (real pheno)
+  # mean_ld <- round(rnorm(n_years, mean_ld_site, 15)) # (real pheno)
+  
+  # for selection/variance explorations
+  mean_ld <- 100
   
   # variance in laying date for each year,
-  sd_ld <- 7
+  # sd_ld <- 7
   
   # can vary between years (then transform as a vector)
   # mean_var_ld <- 10
@@ -45,7 +51,7 @@ simul_data <- function(n_breeders,
   # number of eggs per pair
   # (should depend on the laying date)
   mean_eggs <- 8
-  sd_eggs <- 2
+  # sd_eggs <- 2 # no need when sampling from poisson
   
   
   # final data_set
@@ -55,7 +61,10 @@ simul_data <- function(n_breeders,
                         prod = NA,
                         year = NA)[0,] 
   
+  
   for(k in 1:n_years){
+    
+    ## PHENOLOGY ##
     
     # sample n_breeders laying events
     ld_dates <- round(rnorm(n_breeders, 
@@ -63,10 +72,18 @@ simul_data <- function(n_breeders,
                             sd = sd_ld))# or sd_ld[k]
     
     # fledglings dates (50 = incubation time + rising)
-    fledgl_dates <- round(ld_dates)+50
+    # imply that they all fledge at the same time 
+    
+    fledgl_dates <- round(ld_dates)+40
     
     # number of eggs per pair
-    n_eggs <- abs(round(rnorm(n_breeders, mean_eggs, sd_eggs)))
+    
+    ## FECUNDITY ##
+    
+    n_eggs <- rpois(n_breeders, 
+                    lambda = mean_eggs*exp(selection_stre_ld*ld_dates))
+    
+    # hist(rpois(1000,lambda = mean_eggs*exp(-0.003*ld_dates)), breaks = 100)
     
     
     # create a dataframe (one row per breeding pair)
@@ -76,13 +93,6 @@ simul_data <- function(n_breeders,
       fledgl_dates = fledgl_dates
     )
     
-    # n_eggs as function of ld_dates
-    df_breed <- df_breed%>%
-      mutate(rap = ld_date/(mean_ld[k]*0.9),
-        n_eggs = round(n_eggs*(1/rap)),
-        egg_lost = n_egg - n_eggs)%>%
-      arrange(ld_date)
-      
     
     #### sample (as CES design) #### 
     
@@ -91,7 +101,9 @@ simul_data <- function(n_breeders,
                         length.out = n_session))
     
     # Number of sample individual per session (~capture effort)
-    mean_n_capt <- 9
+    # for now: does not vary along the season
+    
+    mean_n_capt <- 30
     
     # Dataframe with n_adults and n_juveniles captured per session
     df_session <- data.frame(t = t_capt,
@@ -102,24 +114,26 @@ simul_data <- function(n_breeders,
     
     for(i in t_capt){
       
-      # catchable adults
+      # catchable adults (no variation of survival during the season)
       n_adults <- n_breeders * 2
       
-      # catchable juveniles
-      n_juveniles <- sum(df_breed[df_breed$fledgl_date < i ,]$n_eggs)
+      # catchable juveniles (no variation of survival during the season)
+      n_juveniles <- sum(df_breed[df_breed$fledgl_date < i ,]$n_egg)
       
       # sample birds among available individuals
       capt_indiv <- sample( 
         c(rep(0,n_adults), # adults
           rep(1,n_juveniles)), # juveniles
-        round(rnorm(1,mean_n_capt, sd = 3)), # number of capture 
-        replace = TRUE) 
+        rpois(1,mean_n_capt),
+        
+        replace = TRUE # allow recapture
+      ) 
       
-      # adults and juveniles have the same capture probability
-
+      # --> adults and juveniles have the same capture probability
+      
       
       # how many adults
-      df_session[df_session$t ==i,"n_capt_adults"] <- sum(capt_indiv == 0)
+      df_session[df_session$t == i,"n_capt_adults"] <- sum(capt_indiv == 0)
       
       # how many juveniles
       df_session[df_session$t ==i,"n_capt_juveniles"] <- sum(capt_indiv == 1) 
@@ -131,6 +145,7 @@ simul_data <- function(n_breeders,
       mutate(prod = n_capt_juveniles/(n_capt_adults+n_capt_juveniles))
     
     df_site <- rbind(df_site, df_session)
+    
   }
   
   # df with mean ld per year ("real breeding date")
@@ -143,38 +158,177 @@ simul_data <- function(n_breeders,
 }
 
 
-# 
-# sim data with
-data1 <- simul_data(
-  # 5 pairs of breeders per year
-  n_breeders = 5,
-  # 10 years
-  n_years = 10,
-  # CES start (julian days)
-  start_ces = 120,
-  # CES end (julian days)
-  end_ces = 200,
-  # sessions per year
-  n_session = 9,
-  # mean laying date for this site
-  mean_ld_site = 120)
-# 
-# 
-# # plot productivity~day
-# 
-# data1$capt_sess%>%
-#   ggplot(aes(x = t, y = prod, color = as.character(year)))+
-#   geom_point()+
-#   # add leaying dates
-#   geom_vline(data = data1$mean_ld_year,
-#              aes(xintercept = mean_ld,
-#                  color = as.character(year)), alpha = 0.8)+
-#   geom_line(alpha = 0.3)+
-#   theme_light()
+
+# SELECTION ---------------------------------------------------------------
 
 
+# selection = 0
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+                    n_years = 1, 
+                    n_session = 50, 
+                    start_ces = 100,
+                    end_ces = 200,
+                    mean_ld_site = 90,
+                    selection_stre_ld = 0,
+                    sd_ld = 7)
+
+data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()
+
+# selection = -0.003
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+           n_years = 1, 
+           n_session = 50, 
+           start_ces = 100,
+           end_ces = 200,
+           mean_ld_site = 90,
+           selection_stre_ld = -0.003,
+           sd_ld = 7)
+
+data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()
 
 
+# selection = -0.01
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+                    n_years = 1, 
+                    n_session = 50, 
+                    start_ces = 100,
+                    end_ces = 200,
+                    mean_ld_site = 100,
+                    selection_stre_ld = -0.01,
+                    sd_ld = 7)
+
+data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()
 
 
+# VARIANCE LD -------------------------------------------------------------
 
+# HIGHer variance in LD +
+# no selection
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+                    n_years = 1, 
+                    n_session = 50, 
+                    start_ces = 100,
+                    end_ces = 200,
+                    mean_ld_site = 100,
+                    selection_stre_ld = 0,
+                    sd_ld = 12)
+
+high_var_no_sel <- data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()+ theme(legend.position = "none")+
+  ggtitle("selection = 0, sd_layingdate = 12")
+
+# HIGH variance in LD +
+# strong selection
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+                    n_years = 1, 
+                    n_session = 50, 
+                    start_ces = 100,
+                    end_ces = 200,
+                    mean_ld_site = 100,
+                    selection_stre_ld = -0.01,
+                    sd_ld = 12)
+
+high_var_str_sel <- data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()+ theme(legend.position = "none")+
+  ggtitle("selection = -0.01, sd_layingdate = 12")
+
+# LOWer variance in LD +
+# no selection
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+                    n_years = 1, 
+                    n_session = 50, 
+                    start_ces = 100,
+                    end_ces = 200,
+                    mean_ld_site = 100,
+                    selection_stre_ld = 0,
+                    sd_ld = 5)
+
+low_var_no_sel <- data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()+ theme(legend.position = "none")+
+  ggtitle("selection = 0, sd_layingdate = 5")
+
+# LOWer variance in LD +
+# strong selection
+
+data1 <- simul_data(n_breeders = 10, # number of pair
+                    n_years = 1, 
+                    n_session = 50, 
+                    start_ces = 100,
+                    end_ces = 200,
+                    mean_ld_site = 100,
+                    selection_stre_ld = -0.01,
+                    sd_ld = 5)
+
+low_var_str_sel <- data1$capt_sess%>%
+  ggplot(aes(x = t, y = prod, color = as.character(year)))+
+  geom_point()+
+  # add leaying dates
+  geom_vline(data = data1$mean_ld_year,
+             aes(xintercept = mean_ld,
+                 color = as.character(year)), alpha = 0.8)+
+  geom_line(alpha = 0.3)+
+  ylim(c(0,1))+
+  theme_light()+ theme(legend.position = "none")+
+  ggtitle("selection = -0.01, sd_layingdate = 5")
+
+gridExtra::grid.arrange(high_var_str_sel,
+                        low_var_str_sel,
+                        high_var_no_sel,
+                        low_var_no_sel)
